b71550a0027f9b434a636665a7cc6a94108c1970
DongHyeokJeong <ddkkd1@naver.com>
2017-02-15 00:14:26 +0900

    add num object

--- TowerOfHanoi/src/TowerOfHanoi.java
+++ TowerOfHanoi/src/TowerOfHanoi.java
@@ -1,26 +1,35 @@
 import java.util.Scanner;
 
+class Num {
+    int value=0;
+
+    Num(int value) {
+        this.value = value;
+    }
+}
+
 public class TowerOfHanoi {
     public static void main (String [] args) throws Exception {
         int n;
-        int num=0;
         char A = 'A';
         char B = 'B';
         char C = 'C';
+
+        Num num = new Num(0);
 
         Scanner scan = new Scanner(System.in);
         n = scan.nextInt();
 
         movePlate(n,A,B,C,num);
 
-        System.out.println(num);
+        System.out.println(num.value);
     }
 
-    public static void movePlate(int n, char frompeg,char topeg, char auxpeg, int num) {
+    public static void movePlate(int n, char frompeg,char topeg, char auxpeg, Num num) {
+        num.value=num.value+1;
 
         if(n == 1) {
             System.out.println("Move disk " + frompeg + " -> " + topeg);
-            num++;
             return;
         }
         else {
