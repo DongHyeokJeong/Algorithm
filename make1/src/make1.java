905110163d1f9e838b76332d4d4227a6edceead6
DongHyeokJeong <ddkkd1@naver.com>
2017-04-03 15:07:46 +0900

    DP

--- make1/src/make1.java
+++ make1/src/make1.java
@@ -1,5 +1,30 @@
-/**
- * Created by JDH on 2017-04-03.
- */
+import java.util.Scanner;
+
 public class make1 {
+    static int[] dp = new int[1000001];
+
+    public static void main(String [] args) {
+        int p;
+
+        Scanner scan = new Scanner(System.in);      // 문자 입력을 인자로 Scanner 생성
+        p = scan.nextInt();            // 키보드 문자 입력
+
+        dp[1]=0;
+
+        for(int i=2;i<=p;i++) {
+            dp[i] = dp[i-1]+1;
+            if((i%2) == 0) {
+                dp[i] = min(dp[i], dp[i/2]+1);
+            }
+            if((i%3) == 0) {
+                dp[i] = min(dp[i], dp[i/3]+1);
+            }
+        }
+
+        System.out.print(dp[p]);
+    }
+
+    public static int min(int a, int b){
+        return a > b ? b : a;
+    }
 }
