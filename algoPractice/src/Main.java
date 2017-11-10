af56b21888332c891476dc42995f8fe67b4f5cdf
DongHyeokJeong <ddkkd1@naver.com>
2017-11-11 00:39:18 +0900

    알고리즘 연습

--- algoPractice/src/Main.java
+++ algoPractice/src/Main.java
@@ -3,17 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static int[] alpha = new int[200];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //int N = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
+        int group_num=T;
 
-        String s1 = br.readLine().trim();
-        String s2[] = s1.split(" ");
+        for(int i=0;i<T;i++) {
+            String s[] = br.readLine().split("");
 
-        if(s2[0].isEmpty()) {
-            System.out.print(s2.length-1);
-        } else {
-            System.out.print(s2.length);
+            for(int j=97;j<=122;j++) {
+                alpha[j] = -1;
+            }
+
+            for(int j=1;j<s.length;j++) {
+                if(!s[j-1].equals(s[j])) {
+                    if(alpha[(int) s[j].charAt(0)] != -1) {
+                        group_num--;
+                        break;
+                    }
+                    alpha[(int) s[j-1].charAt(0)] = 0;
+                }
+            }
         }
+        System.out.print(group_num);;
     }
 }
